e6ccb5ad53b9b9fd666df48bab35aeeb0bf094f3
Nick536363 <3383371nik@gmail.com>
2025-11-29 10:19:47 +0100

    установил и настроил панель отладки

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -6,6 +6,9 @@ env.read_env()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+INTERNAL_IPS = [
+    "127.0.0.1"
+]
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
@@ -20,8 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'blog',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -32,6 +35,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
 ]
 
 ROOT_URLCONF = 'sensive_blog.urls'
