161e4533bd7b0549733cec3d51bb54fe4a60ab20
Nick536363 <3383371nik@gmail.com>
2025-11-29 11:08:45 +0100

    сократил количество запросов к БД

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
+from django.db.models import Count
 
 
 def get_related_posts_count(tag):
@@ -33,7 +34,7 @@ def serialize_tag(tag):
 
 def index(request):
 
-    most_popular_posts = sorted(Post.objects.all(), key=get_likes_count)[-5:]  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]  # TODO. Как это посчитать?
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
